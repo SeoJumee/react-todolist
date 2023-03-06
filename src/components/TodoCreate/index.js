8033b3045bdcb20dd58f0fe652212c24f2213d0c
서주미 <official.jumee@gmail.com>
2023-03-07 08:18:20 +0900

    TodoCreate

---
+++ src/components/TodoCreate/index.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import * as S from './style';
+import { MdAdd } from 'react-icons/md';
+
+function TodoCreate() {
+  const [open, setOpen] = useState(false);
+
+  const onToggle = () => setOpen(!open);
+  return (
+    <>
+      {open && (
+        <S.InsertFormPositioner>
+          <S.InsertForm>
+            <S.Input
+              autoFocus
+              placeholder="할 일을 입력 후, Enter 를 누르세요"
+            />
+          </S.InsertForm>
+        </S.InsertFormPositioner>
+      )}
+      <S.CircleButton onClick={onToggle} open={open}>
+        <MdAdd />
+      </S.CircleButton>
+    </>
+  );
+}
+
+export default TodoCreate;
